b95cd2d8cee4456c3fe1e594f301b18b556054ec
IOS-刘瑞杰 <liuruijie@gracepsy.com>
2018-05-16 14:40:41 +0800

    flsaldf

---
+++ bletest/bletest/go/kdlsjfl.h
@@ -0,0 +1,13 @@
+//
+//  kdlsjfl.h
+//  bletest
+//
+//  Created by 刘瑞杰 on 2018/5/16.
+//  Copyright © 2018年 刘瑞杰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface kdlsjfl : NSObject
+
+@end
---
+++ bletest/bletest/go/kdlsjfl.m
@@ -0,0 +1,13 @@
+//
+//  kdlsjfl.m
+//  bletest
+//
+//  Created by 刘瑞杰 on 2018/5/16.
+//  Copyright © 2018年 刘瑞杰. All rights reserved.
+//
+
+#import "kdlsjfl.h"
+
+@implementation kdlsjfl
+
+@end
