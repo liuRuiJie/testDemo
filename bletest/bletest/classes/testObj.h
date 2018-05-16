cc195259e6d528830cd47309f519d70cb7ac8f06
IOS-刘瑞杰 <liuruijie@gracepsy.com>
2018-05-16 13:40:27 +0800

    fig

---
+++ bletest/bletest/classes/testObj.h
@@ -0,0 +1,13 @@
+//
+//  testObj.h
+//  bletest
+//
+//  Created by 刘瑞杰 on 2018/5/16.
+//  Copyright © 2018年 刘瑞杰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testObj : NSObject
+
+@end
---
+++ bletest/bletest/classes/testObj.m
@@ -0,0 +1,13 @@
+//
+//  testObj.m
+//  bletest
+//
+//  Created by 刘瑞杰 on 2018/5/16.
+//  Copyright © 2018年 刘瑞杰. All rights reserved.
+//
+
+#import "testObj.h"
+
+@implementation testObj
+
+@end
